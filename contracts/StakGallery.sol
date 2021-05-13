d179ecb3989581b68eef40bf412d4d605de3b763
canokaue <kauecano@gmail.com>
2021-05-13 12:44:02 -0300

    add eERC-721.sol

--- contracts/StakGallery.sol
+++ contracts/StakGallery.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.7.1;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "./token/eERC721.sol";
+import "./access/Ownable.sol";
+import "./utils/Counters.sol";
 
 contract StakGallery is ERC721, Ownable {
     using Counters for Counters.Counter;
